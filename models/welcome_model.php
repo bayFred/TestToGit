5157347da615b00cd01e21049b66427b44ce68be
bayFred <41265666+bayFred@users.noreply.github.com>
2018-07-18 08:26:50 +0300

    first commit

--- models/welcome_model.php
+++ models/welcome_model.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        return 'hello from  <b>'.__FILE__;
+        $welView =  'hello from  <b>'.__FILE__;
         ?>
     </body>
 </html>
